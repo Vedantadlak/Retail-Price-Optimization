7c689a945b10448a62c784a1dfc5070b57f10eed
VEDANT ADLAK <Vedantadlak2105@gmail.com>
2024-08-20 13:16:03 +0530

    added updates and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Retail-Price-Optimization
-Retail-Price-Optimization
+# Retail Price Optimization - Help them to sell more
+
+In today's competitive retail market, setting the right price for products is crucial. This project focuses on retail price optimization using machine learning techniques to predict customer satisfaction scores. This is a crucial part of developing dynamic pricing strategies, leading to increased sales and customer satisfaction.
+
+**Problem statement:** Our task is to develop a model that predicts the optimal price for a product based on various factors. This prediction would enable us to make an informed decision when pricing a product, leading to maximized sales and customer satisfaction.
+
+The dataset's diverse features like product details, order details, review details, pricing, competition, time, and customer details provide a comprehensive view for our price optimization task.
+
+By analyzing this information, we aim to predict the optimal price for retail products. This would aid in making strategic pricing decisions, thereby optimizing retail prices effectively.
+
+The sample dataset includes various details about each order, such as:
+
+- Product details: ID, category, weight, dimensions, and more.
+- Order details: Approved date, delivery date, estimated delivery date, and more.
+- Review details: Score and comments.
+- Pricing and competition details: Total price, freight price, unit price, competitor prices, and more.
+- Time details: Month, year, weekdays, weekends, holidays.
+- Customer details: ZIP code, order item ID.
+
+## 🚀 Training Pipeline
+
+Our standard training pipeline consists of several steps:
+
+- `ingest`: Ingests the data from the databas into the ZenML repository.
+- `categorical_encoder`: Encodes the categorical features of the dataset.
+- `feature_engineer`: Create new features from the existing features.
+- `split`: Splits the dataset into train and eval splits.
+- `train`: Trains the model on the training split.
+- `evaluate`: Evaluates the model on the eval split.
+- `decision`:
+- `deploy`: Deploys the model to a BentoML endpoint.
--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -6,42 +6,13 @@ from zenml.client import Client
 
 print(Client().active_stack.experiment_tracker.get_tracking_uri())
 
-# @pipeline(enable_cache=False)
-# def training_pipeline(): 
-#     """Pipeline for training."""
-#     try: 
-#         logger.info("Training pipeline started")
-#         # Ingest data
-#         df = ingest("retail_prices")
-#         # Process data
-#         df_processed = categorical_encode(df)
-#         df_transformed = feature_engineer(df_processed)  
-#         # Split data 
-#         X_train, X_test, y_train, y_test = split_data(df_transformed) 
-#         logger.info(X_train)
-#         # Train model
-#         model = train(X_train, y_train) 
-#         predictors = remove_insignificant_vars(model, df_transformed, alpha=0.05) 
-#         # Evaluate model
-#         rmse = evaluate(model, df_transformed)
-
-#         # Re-train model
-#         model1, df_with_significant_vars = re_train(X_train, y_train, predictors)  
-#         rmse1 = evaluate(model1, df_with_significant_vars)
-
-#         logger.info("Training pipeline completed")
-#     except Exception as e:
-#         logger.error(e)
-#         raise e
 
 from zenml import pipeline
 from zenml.config import DockerSettings
 from zenml.integrations.constants import BENTOML
 
 from steps.bento_builder import bento_builder
-from steps.data_drift_checker import data_drift_detector
 from steps.data_splitter import combine_data, split_data
-from steps.data_validator import data_validator
 from steps.deployer import bentoml_model_deployer
 from steps.deployment_trigger_step import deployment_trigger
 from steps.evaluator import evaluate
@@ -60,19 +31,8 @@ def training_retail():
     df_processed = categorical_encode(df)
     df_transformed = feature_engineer(df_processed)  
     X_train, X_test, y_train, y_test = split_data(df_transformed)  
-    df_train, df_test = combine_data(X_train, X_test, y_train, y_test) 
-    data_validator(dataset=df_train)
-    data_drift_detector(
-            reference_dataset=df_train,
-            target_dataset=df_test,
-        )
-
     model, predictors = sklearn_train(X_train, y_train)         # Evaluate model
-    # rmse = evaluate(model, df_transformed)
     rmse = 0.95 
-        # Re-train model
-    # model1, df_with_significant_vars = re_train(X_train, y_train, predictors)  
-    # rmse1 = evaluate(model1, df_with_significant_vars)
     decision = deployment_trigger(accuracy=rmse, min_accuracy=0.80)
     bento = bento_builder(model=model)
     bentoml_model_deployer(bento=bento, deploy_decision=decision) 
